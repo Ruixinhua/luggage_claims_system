72f632dbd87525c68497982fac7175afad0907b7
Liu Dairui <rxhjxja@gmail.com>
2019-03-25 00:46:17 +0800

    Create back-end framework using Spring boot

---
+++ src/main/java/com/spring/boot/luggage_claims_system/hirbernia_sina/HirberniaSinaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.luggage_claims_system.hirbernia_sina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirberniaSinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirberniaSinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/boot/luggage_claims_system/hirbernia_sina/HirberniaSinaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.boot.luggage_claims_system.hirbernia_sina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HirberniaSinaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
