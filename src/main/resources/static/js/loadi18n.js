eb442c22f757eafe48522c3a1e18463605e07eea
RandomName9 <ielts0037@163.com>
2019-04-07 17:24:55 +0800

    添加获取语言的函数,修改strings中的错误
    调用getLanguageCode() 获取当前语言
    结果为字符串只可能为en或zh

--- src/main/resources/static/js/loadi18n.js
+++ src/main/resources/static/js/loadi18n.js
@@ -4,10 +4,9 @@
 		function loadProperties(type) {
 			//alert("load "+type);
 			$.cookie('Glangcode', type,{ expires: 30,path:'/',secure:false });
-			//$.cookie('langcode', type,{ expires: 30,secure:false });
 			jQuery.i18n.properties({
 				name: 'strings', 
-				path: '/js/i18n',
+				path: '/js/i18n', 
 				mode: 'map', 
 				language: type, 
 				cache: false,
@@ -19,17 +18,6 @@
 							arr[i].innerText=$.i18n.prop(LangId);
 					}
 					
-//					arr = $("[i18nid_placeholder]");		
-//					for (i = 0; i < arr.length; i++) {
-//							LangId=arr[i].getAttribute('i18nid_placeholder');
-//							arr[i].setAttribute('placeholder',$.i18n.prop(LangId));
-//					}
-//					
-//					arr = $("[i18nid_title]");		
-//					for (i = 0; i < arr.length; i++) {
-//							LangId=arr[i].getAttribute('i18nid_title');
-//							arr[i].setAttribute('title',$.i18n.prop(LangId));
-//					}
 					Replacei18nAttr("placeholder");
 					Replacei18nAttr("title");
 					
@@ -55,15 +43,23 @@
 				
 		}
 
+
+		function getLanguageCode()
+		{
+			var LangcodeFromCookie=$.cookie('Glangcode');
+			LangcodeFromCookie=LangcodeFromCookie=='en'?'en':'zh';
+			return LangcodeFromCookie;
+		}
+
 		function switchLang() {
 			LANGUAGE_CODE = LANGUAGE_CODE == 'zh' ? 'en' : 'zh';
 			loadProperties(LANGUAGE_CODE);
 		}
 
 		$(document).ready(function () {
-			var LangcodeFromCookie=$.cookie('Glangcode');
-			//alert('cookie:'+LangcodeFromCookie);
-			LangcodeFromCookie = LangcodeFromCookie == 'zh' ? 'zh' : 'en';
+			var LangcodeFromCookie=getLanguageCode();
+		//	var LangcodeFromCookie=$.cookie('Glangcode');
+			//LangcodeFromCookie=LangcodeFromCookie=='en'?'en':'zh';
 			loadProperties(LangcodeFromCookie);
 			
 			
